765c9be3a40e121390801503c43ce87afc177f43
perry1224 <perry.w1224@gmail.com>
2023-04-25 23:47:59 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,28 @@ return (
     <section className="side-bar">
       <button>+ New Chat</button>
       <ul className="history">
+          <li>Hello</li>
+      </ul>
         <nav>
+      
           <p>Made by Perry</p>
         </nav>
-      </ul>
+   
     </section>
-    <section className="main"></section>
+
+    <section className="main">
+      <h1>Chat with Perry</h1>
+      <ul className="feed">
+      </ul>
+      
+      <div className="bottom-section">
+        <div className="input-container">
+          <input/>
+          <div id="submit">➢</div>
+        </div>
+        <p className="info"> Testing</p>
+      </div>
+    </section>
   </div>
 )
 }
