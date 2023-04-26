caa9f19460066188fbb6515e58544e413dda4dd6
perry1224 <perry.w1224@gmail.com>
2023-04-25 23:02:22 -0400

    creating files

---
+++ chatgptclone/src/App.js
@@ -0,0 +1,17 @@
+const App = () => {
+return (
+  <div className="app">
+    <section className="side-bar">
+      <button>+ New Chat</button>
+      <ul className="history">
+        <nav>
+          <p>Made by Perry</p>
+        </nav>
+      </ul>
+    </section>
+    <section className="main"></section>
+  </div>
+)
+}
+
+export default App;
